7b2df2d06dfd64ae2f61fb6ddef20fec6ec05e1d
naeemul-online <naeemul.online@gmail.com>
2024-02-12 22:14:48 +0300

    first commit

---
+++ geometrics-genius.js
@@ -0,0 +1,23 @@
+/* 
+step 1: get base value ot the triangle.
+step 2: added an id in the base input field
+step 3: use get element by id to access the input field.
+step 4: get the base value . now it is string
+step 5: convert the base value to number. using parse float method.
+step 6: Now use the formula to get the result ;
+step 7: set output in the Area calculator section use innerHTML
+step 8: make empty input after click the input button use value = '';
+*/
+function triAngleCalculate(){
+    const base = document.getElementById('triangle-base')
+    const baseValue = base.value;
+    const baseInput = parseFloat(baseValue)
+
+    const height = document.getElementById('triangle-height');
+    const heightValue = height.value;
+    const heightInput = parseFloat(heightValue)
+    const areaTriangle = `Area(A) = ${0.5} x ${baseInput} x ${heightInput} \n Area = ${0.5 * baseInput * heightInput}`
+    document.getElementById("cal-value").innerText = areaTriangle;
+    base.value = '';
+    height.value = '';
+}
